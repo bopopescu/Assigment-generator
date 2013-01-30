9b8fd73eda82e41698b0b9ab469471356116c9a9
Ales Tomecek <TomecekAles@seznam.cz>
2013-01-30 18:05:14 +0100

    pridan globalni scope

--- Ita/generator.py
+++ Ita/generator.py
@@ -2,41 +2,60 @@ from ita_parser import Parser
 from collections import namedtuple
 from random import choice
 from copy import copy
+import builtins 
 VERBOSE = False
 
 
 class Generator:
+    currentScope = None
+    
     def __init__(self, rules):
         def convertRules(rules):
             def wrap(nonterm):
                 def wrapper(*args, **kwargs):
                     # todo check number of params
+                    # todo nacti defaultni params
                     localNonterm = nonterm
                     selected = choice( rules[ localNonterm ] )
 
-                    glob = {}
+                    glob = self.getScope()
                     loc = {}
-                    print(selected.nonterm)
-                    print(selected.info)
+                    print(localNonterm);
                     exec(selected.code, glob, loc)
-                    return loc["implementation"](*args, **kwargs)
-                return wrapper
 
+                    retVal =  loc["implementation"](*args, **kwargs)
+
+                    return retVal
+                
+                return wrapper
+ 
             calls = {}
 
             for nonterm in rules:
-                print("dipl "+nonterm)
-
                 calls[ nonterm ] = wrap(nonterm) 
 
             return calls
         
         self.rules = convertRules( rules )
 
+    def getScope(self):
+        if self.currentScope == None:
+            self.currentScope = { '__builtins__' : builtins  }
+        return self.currentScope
+
+    def clearScope(self):
+        self.currentScope = None
+
+    def run(self, startNonterminal):
+        self.clearScope()
+        return self.rules[startNonterminal]()
+        
+        
+
 p = Parser()
 
 p.loadDir("base")
-#p.loadDir("cviceni3")
+p.loadDir("cviceni3")
 
 g = Generator( p.rules )
-print(g.rules["cislo"](0,5))
+print(g.run("cviceni"))
