d12b451795847e4c12aecc5bb17d64c723c52b0d
Ales Tomecek <TomecekAles@seznam.cz>
2013-04-17 18:49:48 +0200

    reloader ceka na zmenu souboru NEBO na stisknutou klavesu

--- ita/cli/reloader.py
+++ ita/cli/reloader.py
@@ -1,6 +1,43 @@
 from .helpers import consoleFriendly
 from time import sleep
 import os.path
+import threading
+import _thread
+
+class WaitingThread(threading.Thread):
+        
+    def __init__(self, event, sleep = 1):
+        super(WaitingThread, self).__init__()
+        # daemon thread se při ukončeni main vlakna vynuceně ukončí
+        # něco co python nepodporuje    
+        self.daemon = True
+        self._event = event
+        self._sleep =  sleep
+        
+        self.fired = False
+        
+
+    def run(self):
+        sleep(self._sleep)
+        self.fired = True
+        self._event.set()
+
+class InputThread(threading.Thread):
+        
+    def __init__(self, event):
+        super(InputThread, self).__init__()
+        self.daemon = True
+        self._event = event
+        
+        self.fired = False
+
+    def run(self):
+        input()
+        self.fired = True
+        self._event.set()
+
+
+          
 
 def run(nonterminal, interval, path, toAscii = True):
     import ita
@@ -10,6 +47,12 @@ def run(nonterminal, interval, path, toAscii = True):
     print("Pro ukonceni testovaciho rezimu pouzijte CTRL+C")
     if toAscii:
         print("Vystup bude preved do cisteho ASCII")
+
+    # event pro synchronizaci změny
+    changed = threading.Event()
+    
+    # vlakno pro odchytavani zmacknuti enter
+    inputing = None
 
     while True:
         try:
@@ -27,14 +70,47 @@ def run(nonterminal, interval, path, toAscii = True):
         
             filesToBeWatched = { fileName : os.path.getmtime(fileName) for fileName, data in l.getPathsOnly() }
 
-            changed = False
-            while not changed:
-                sleep(interval)
-                for fileName, mtime in filesToBeWatched.items():
-                    if os.path.getmtime(fileName) != mtime:
-                        changed = True
-                        break
+            
+            waiting = WaitingThread(changed, interval)
+            waiting.start()
+            
+            if not inputing:
+                # vlákno inputing muže existovat, 
+                # input nelze spustit 2x (jde ale bude se čekat na ukončeni prvniho) a pokud dojde k reload z duvodu změny souboru
+                # puvodni input porad bezi
+                # to je take duvod proc se pozuziva stejny event
+                inputing = InputThread(changed)
+                inputing.start()
+            
+            immortal = True
+            while immortal:
+                #čekáme na změnu
+                changed.wait()
+            
+                # zjistíme kdo změnil stav
+                
+                if waiting.fired: # čekací vlákno vyvolalo vyjímku  => timeout
+                    for fileName, mtime in filesToBeWatched.items():
+                        if os.path.getmtime(fileName) != mtime:
+                            immortal = False
+                            break
+                    #restartujeme cekani
+                    del waiting
+                    waiting = WaitingThread(changed, interval)
+                    waiting.start()   
+                
+                elif inputing.fired: # vlákno vstupu => keypress
+                    #restartovat ho nemá cenu, protože bude stejně přepsáno
+                    immortal = False
+                    #force smazani
+                    del inputing
+                    #nastavime, aby se priste smazal 
+                    inputing = None
+            
+            changed.clear()      
+            
             print("-"*20)
+                    
         except KeyboardInterrupt:
             print("-"*20)
             break 
