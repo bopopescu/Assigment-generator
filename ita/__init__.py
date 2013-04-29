7b3891857ab66c44ba0f7fd59900b1ab65466d01
Ales Tomecek <TomecekAles@seznam.cz>
2013-04-29 10:22:04 +0200

    přidána cesta k modulu a přímý odkaz na file loader

--- ita/__init__.py
+++ ita/__init__.py
@@ -4,9 +4,12 @@ from ita import loader_file
 from ita import parser
 from ita import generator
 
+import os
+MODULE_PATH = os.path.dirname(__file__)
 
+FileLoader = loader_file.FileLoader
 Loader = loader_file.FileLoader
 Parser = parser.Parser
 Generator = generator.Generator
 
-__all__ = ["VERBOSE", "web", "cli", "Generator", "Parser", "Loader"] 
+__all__ = ["VERBOSE", "MODULE_PATH", "web", "cli", "Generator", "Parser", "Loader", "FileLoader"] 
