ccb338d0df1df3dc46691bacb36b7ec2e8780236
Ales Tomecek <TomecekAles@seznam.cz>
2013-05-12 19:21:37 +0200

    reloader slug nezkolabuje pri predanem None

--- ita/cli/helpers.py
+++ ita/cli/helpers.py
@@ -2,6 +2,8 @@ import unicodedata
 import re
 
 def consoleFriendly(val):
-    """Převede string na znaky bez diakritiky""" 
+    """Převede string na znaky bez diakritiky"""
+    # vstup může být None, na něm by dekodovani zkolabovalo
+    if not val: return "" 
     ret = unicodedata.normalize('NFKD', val).encode('ascii', 'ignore').decode('utf-8')
     return ret
